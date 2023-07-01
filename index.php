bc260f7f214e76fdd86365ad56a23050700d5abf
mirkoMac <mirkomimic88@gmail.com>
2023-07-01 19:51:19 +0200

    comments

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
 
-use Seytar\Routing\Router;
-use Illuminate\Support\Facades\Route;
+// use Seytar\Routing\Router;
+// use Illuminate\Support\Facades\Route;
 
 require_once './vendor/autoload.php';
 
 session_start();
 
-Router::bootstrap(function ($ex) {
-  header('Content-Type: text/html; charset=utf-8');
-  echo '404 - Page Not Found';
-});
+// Router::bootstrap(function ($ex) {
+//   header('Content-Type: text/html; charset=utf-8');
+//   echo '404 - Page Not Found';
+// });
 
-Route::get('/', function () {
-  echo 'Hello world.';
-});
+// Route::get('/', function () {
+//   echo 'Hello world.';
+// });
 
 
 // login
